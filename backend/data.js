b5062f818ee462c5961cf3113acad3f05e6291c8
Ishaq Mohammad <109386487+ishaaqji@users.noreply.github.com>
2026-03-27 05:18:22 +0530

    Create data.js

---
+++ backend/data.js
@@ -0,0 +1,10 @@
+let products = [];
+
+let orders = [];
+
+let partners = [
+  { id: 1, name: "Ravi", lat: 17.385, lng: 78.486 },
+  { id: 2, name: "Ali", lat: 17.400, lng: 78.480 }
+];
+
+module.exports = { products, orders, partners };
